a1105dc63cb6355ce07c34c54737581fa405232e
AhmadHRai <ahmadhalraie@gmail.com>
2023-06-14 13:08:00 +0300

    Testing access to files in the same package

--- src/Main.java
+++ src/Main.java
@@ -10,5 +10,9 @@ public class Main {
             // for you, but you can always add more by pressing Ctrl+F8.
             System.out.println("i = " + i);
         }
+
+        String name = test.testFunc("Ahmad");
+        System.out.println(name);
+
     }
 }
---
+++ src/test.java
@@ -0,0 +1,8 @@
+public class test {
+
+    public static String testFunc(String name) {
+        return "Hello " + name;
+
+    }
+
+}
